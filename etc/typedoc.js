d69ce1086ca3b820c8beb72f049703df00719447
Cédric Belin <cedric@belin.io>
2024-06-03 16:29:02 +0200

    Add some configuration files

---
+++ etc/typedoc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {Partial<import("typedoc").TypeDocOptions>}
+ */
+export default {
+	entryPoints: ["../src/index.js"],
+	excludePrivate: true,
+	gitRevision: "main",
+	hideGenerator: true,
+	name: "Eta for Koa",
+	out: "../docs/api",
+	readme: "none",
+	tsconfig: "../src/tsconfig.json"
+};
