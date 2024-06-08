41f56cf59673598c6d1d2427fae9bbd1b60de280
Cédric Belin <cedric@belin.io>
2024-06-08 23:13:13 +0200

    Update the "test" task [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 import {cp, readFile, writeFile} from "node:fs/promises";
 import {EOL} from "node:os";
+import {env} from "node:process";
 import {deleteAsync} from "del";
 import {$} from "execa";
 import gulp from "gulp";
@@ -44,6 +45,7 @@ export async function serve() {
 
 // Runs the test suite.
 export function test() {
+	env.NODE_ENV = "test";
 	return $`node --test --test-reporter=spec`;
 }
 
