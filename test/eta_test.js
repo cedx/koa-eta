1a392e637feaa6cb03697f3399f4e898733cb746
Cédric Belin <cedric@belin.io>
2024-06-09 02:15:09 +0200

    Add some unit tests

---
+++ test/eta_test.js
@@ -0,0 +1,45 @@
+import {equal, ok} from "node:assert/strict";
+import {after, before, describe, it} from "node:test";
+import app from "../example/app.js";
+
+/**
+ * Tests the features of the {@link eta} function.
+ */
+describe("eta()", () => {
+	const url = new URL("http://127.0.0.1:3000/");
+	const controller = new AbortController;
+	before(() => app.listen({host: url.hostname, port: Number(url.port), signal: controller.signal}));
+	after(() => controller.abort());
+
+	describe("render()", () => {
+		it("should have been added to the application context", () =>
+			ok(typeof app.context.render == "function"));
+
+		it("should render a view as HTML page", async () => {
+			const response = await fetch(url, {headers: {accept: "text/html"}});
+			equal(response.headers.get("content-type"), "text/html; charset=utf-8");
+			equal(response.status, 200);
+
+			const body = await response.text();
+			ok(body.startsWith("<!DOCTYPE html>"));
+			ok(body.includes("<title>Eta for Koa</title>"));
+			ok(body.trimEnd().endsWith("</html>"))
+		});
+	});
+
+	describe("renderPdf()", () => {
+		it("should have been added to the application context", () =>
+			ok(typeof app.context.renderPdf == "function"));
+
+		it("should render a view as PDF document", async () => {
+			const response = await fetch(url, {headers: {accept: "application/pdf"}});
+			equal(response.headers.get("content-type"), "application/pdf");
+			equal(response.status, 200);
+
+			const body = await response.text();
+			ok(body.startsWith("%PDF-"));
+			ok(body.includes("/Title (Eta for Koa)"));
+			ok(body.trimEnd().endsWith("%%EOF"));
+		});
+	});
+});
