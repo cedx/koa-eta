4f68678a688ed0e7ecbb82a6a1842153ea5e4281
Cédric Belin <cedric@belin.io>
2024-08-30 15:01:12 +0200

    Add the Haxelib configuration [skip ci]

--- example/server.js
+++ example/server.js
@@ -11,7 +11,7 @@ const app = new Koa;
 eta(app, {
 	cache: app.env == "production",
 	debug: app.env != "production",
-	views: join(import.meta.dirname, "../res")
+	views: join(import.meta.dirname, "../share/res")
 });
 
 // Configure the data shared by all views.
