5a46e3a02105967c97cd985a59acbd89402edb28
Cédric Belin <cedric@belin.io>
2024-08-31 15:32:39 +0200

    Update the Koa externs [skip ci]

--- src/js/koa/Application.hx
+++ src/js/koa/Application.hx
@@ -1,27 +1,23 @@
 package js.koa;
 
 import js.html.AbortSignal;
+import js.node.http.Server;
+import js.node.net.Server.ServerListenOptionsTcp;
 
-/** TODO **/
+/** Represents a Koa application. **/
 @:jsRequire("koa")
 extern class Application {
 
-	/** TODO **/
+	/** The prototype from which the request context is created. **/
 	final context: Context;
 
-	/** TODO **/
-	function listen(options: ListenOptions): Void;
+	/** Creates and returns an HTTP server, passing the given arguments to `Server.listen()`. **/
+	function listen(options: ListenOptions): Server;
 }
 
 /** Defines the options of the `Application.listen()` method. **/
-typedef ListenOptions = {
+typedef ListenOptions = ServerListenOptionsTcp & {
 
-	/** TODO **/
-	var host: String;
-
-	/** TODO **/
-	var port: Int;
-
-	/** TODO **/
+	/** A signal that may be used to close a listening server. **/
 	var signal: AbortSignal;
 }
