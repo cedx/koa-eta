a8fd7f4ac54ad4a4d873d1e4e917dc53d44be329
Cédric Belin <cedric@belin.io>
2024-08-31 20:17:51 +0200

    Finalize the Haxe port

--- src/js/koa/Context.hx
+++ src/js/koa/Context.hx
@@ -1,7 +1,5 @@
 package js.koa;
 
-import haxe.DynamicAccess;
-
 /** Encapsulates the Node.js `request` and `response` objects into a single object. **/
 extern class Context {
 
@@ -9,7 +7,7 @@ extern class Context {
 	var body: Any;
 
 	/** The namespace for passing information through middleware and to the views. **/
-	var state: DynamicAccess<Dynamic>;
+	var state: {};
 
 	/** The response media type. **/
 	var type: String;
