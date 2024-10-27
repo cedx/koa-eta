32ab3e10a637559a9a3ac93966ab9d4efc7a222f
Cédric Belin <cedric@belin.io>
2024-10-27 13:46:21 +0100

    Replace "esbuild" by "terser" for minification

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -4,5 +4,5 @@ using Lambda;
 function main() {
 	final file = "lib/bundle.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	Sys.command('npx esbuild --allow-overwrite --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
+	Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
 }
