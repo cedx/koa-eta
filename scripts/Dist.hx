867408b4a2767ac0636b2171afafa1df23141ce5
Cédric Belin <cedric@belin.io>
2024-11-02 01:33:09 +0100

    Port the build system to Cake

--- scripts/Dist.hx
+++
@@ -1,8 +0,0 @@
-using Lambda;
-
-/** Packages the project. **/
-function main() {
-	final file = "lib/bundle.js";
-	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
-	Sys.command('npx terser --comments=false --compress --mangle --output=$file $file');
-}
