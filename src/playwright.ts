237e5bcee0148b332ce868181c0fadcefaf4c19c
Cédric Belin <cedric@belin.io>
2025-01-30 11:37:24 +0100

    Code optimization [skip ci]

--- src/playwright.ts
+++ src/playwright.ts
@@ -1,6 +1,6 @@
 import {Buffer} from "node:buffer";
 import {chromium} from "playwright";
-import type {LaunchOptions} from "playwright-core";
+import type {LaunchOptions, Page} from "playwright-core";
 
 /**
  * Converts the specified HTML code into a PDF document.
@@ -20,80 +20,4 @@ export async function htmlToPdf(html: string, options: {browser?: LaunchOptions,
 /**
  * Defines the PDF rendering options.
  */
-export type PdfOptions = Partial<{
-
-	/**
-	 * Value indicating whether to display the header and footer.
-	 */
-	displayHeaderFooter: boolean;
-
-	/**
-	 * The HTML template for the print footer.
-	 */
-	footerTemplate: string;
-
-	/**
-	 * The paper format.
-	 */
-	format: string;
-
-	/**
-	 * The HTML template for the print header.
-	 */
-	headerTemplate: string;
-
-	/**
-	 * The paper height.
-	 */
-	height: number|string;
-
-	/**
-	 * Value indicating the landscape orientation.
-	 */
-	landscape: boolean;
-
-	/**
-	 * The paper margins.
-	 */
-	margin: {bottom?: number|string, left?: number|string, right?: number|string, top?: number|string};
-
-	/**
-	 * Value indicating wether to embed the document outline into the PDF.
-	 */
-	outline: boolean;
-
-	/**
-	 * The paper ranges to print.
-	 */
-	pageRanges: string;
-
-	/**
-	 * The file path to save the PDF to.
-	 */
-	path: string;
-
-	/**
-	 * Value indicating whether to give prority to any CSS `@page` size declared in the page.
-	 */
-	preferCSSPageSize: boolean;
-
-	/**
-	 * Value indicating whether to print the background graphics.
-	 */
-	printBackground: boolean;
-
-	/**
-	 * The scale of the webpage rendering.
-	 */
-	scale: number;
-
-	/**
-	 * Value indicating whether to generate tagged (accessible) PDF.
-	 */
-	tagged: boolean;
-
-	/**
-	 * The paper width.
-	 */
-	width: number|string;
-}>;
+export type PdfOptions = Parameters<Page["pdf"]>[0];
