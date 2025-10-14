4273c738cc5d5be17154b744dff4d1d53957aad4
Cédric Belin <cedx@outlook.com>
2025-10-14 08:26:37 +0200

    Code optimization [skip ci]

--- tool/Test.ps1
+++ tool/Test.ps1
@@ -1,3 +1,3 @@
-Write-Output "Running the test suite..."
+"Running the test suite..."
 npx tsc --build src/tsconfig.json --sourceMap
 Start-Process node "--enable-source-maps --test" -Environment @{ NODE_ENV = "test" } -NoNewWindow -Wait
